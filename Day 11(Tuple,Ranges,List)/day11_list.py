7a58547e5e89d213ec627d25faf03c72371c6811
davidsonoye <148563374+davidsonoye@users.noreply.github.com>
2025-04-22 01:49:21 +0100

    day11_list.py

---
+++ Day 11(Tuple,Ranges,List)/day11_list.py
@@ -0,0 +1,4 @@
+fruits = ['apple', 'banana', 'cherry']
+fruits.append('date')      # Add an item
+fruits[1] = 'blueberry'    # Modify an item
+print(fruits)              # Output: ['apple', 'blueberry', 'cherry', 'date']
