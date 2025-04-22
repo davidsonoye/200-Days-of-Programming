5b60c71057a9bcd4a4aea2f506765953579b5a90
davidsonoye <148563374+davidsonoye@users.noreply.github.com>
2025-04-22 01:51:18 +0100

    day11_ranges.py

---
+++ Day 11(Tuple,Ranges,List)/day11_ranges.py
@@ -0,0 +1,7 @@
+# Print numbers from 0 to 4
+for i in range(5):
+    print(i)
+
+# Create a list of even numbers
+even_numbers = list(range(0, 10, 2))
+print(even_numbers)  # Output: [0, 2, 4, 6, 8]
